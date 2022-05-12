9b5802d3f2df285cfd821196faef3d25dee8c1a4
VallereeJoseph <98525584+VallereeDJoseph@users.noreply.github.com>
2022-05-12 13:43:58 +0800

    Add files via upload

---
+++ Midterms/Exercise15.js
@@ -0,0 +1,20 @@
+var myName="Joseph, Valleree D."
+var myAge="19"
+var myPhoneNumber="09102991632"
+var myEmail="20212590@s.ubaguio.edu"
+var myAddress="412 Purok 4A Lucnab, Baguio City"
+var myProgram="Computer Science"
+var myID="20212590"
+var myYear="1st Year"
+var mySection="IAA1"
+
+console.log('Hello');
+console.log('First Name: '+ myName);
+console.log('Age: '+ myAge);
+console.log('Phone Number: '+ myPhoneNumber);
+console.log('Email Address: '+ myEmail);
+console.log('Mailing Address: '+ myAddress);
+console.log('Program: '+ myProgram);
+console.log('ID Number: '+ myID);
+console.log('Year Level: '+ myYear);
+console.log('Section: '+ mySection);
