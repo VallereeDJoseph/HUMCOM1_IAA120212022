48f9cec98dd52b162f5cfad811726ca64ddcf570
VallereeJoseph <98525584+VallereeDJoseph@users.noreply.github.com>
2022-05-13 12:56:43 +0800

    Add files via upload

---
+++ Midterms/FinalExam/FinalExam.js
@@ -0,0 +1,21 @@
+const myName="Joseph, Valleree Joseph"
+const myEmail="20212590@s.ubaguio.edu"
+const myProgram="Computer Science"
+const myID="20212590"
+const myYear="1rst Year"
+const mySection="IAA1"
+
+console.log('Hello');
+console.log('Name: ' + myName);
+console.log('Email Address: ' + myEmail);
+console.log('Program: ' + myProgram);
+console.log('ID Number: ' + myID);
+console.log('Year Level: ' + myYear);
+console.log('Section: ' + mySection);
+
+var myWebsite="Your Baguio Trip"
+var founder="Joseph, Valleree D."
+
+console.log('Final Project/Final Exam');
+console.log('Website Name: ' + myWebsite);
+console.log('Founder: ' + founder);
